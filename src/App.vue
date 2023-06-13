d8774ae8d7c0389405de6997dd6d5634a3f9f173
peppeluc22 <gluca0260@gmail.com>
2023-06-13 16:47:35 +0200

    made initial front-office

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,60 @@
-<script setup>
-
-</script>
 
 <template>
-  <h1>
+  <div class="container mt-5">
+    <div class="row row-gap-4">
+      <CardComponent v-for="(project, index) in projects" :key="index" :project="project" />
 
-  </h1>
+      <nav aria-label="Page navigation example">
+        <ul class="pagination">
+          <li class="page-item"><button :class="{ 'page-link': true, 'disabled': currentPage === 1 }"
+              @click="getProjects(currentPage - 1)">Previous</button></li>
+          <li class="page-item" v-for="n in lastPage"><button :class="{ 'page-link': true, 'active': currentPage === n }"
+              @click="getProjects(n)">{{ n }}</button>
+          </li>
+
+          <li class="page-item"><button :class="{ 'page-link': true, 'disabled': currentPage === 2 }"
+              @click="getProjects(currentPage + 1)">Next</button></li>
+        </ul>
+      </nav>
+    </div>
+  </div>
 </template>
 
-<style scoped></style>
+
+<script>
+import axios from 'axios';
+import CardComponent from './components/CardComponent.vue';
+export default {
+  name: 'App',
+  components: {
+    CardComponent
+  },
+  data() {
+    return {
+      apiUrl: 'http://127.0.0.1:8000/api',
+      projects: [],
+      currentPage: 1,
+      lastPage: null
+    }
+  },
+  methods: {
+    getProjects(numPage) {
+      axios.get(`${this.apiUrl}/projects`, {
+        params: {
+          'page': numPage
+        }
+      }).then((res) => {
+        this.projects = res.data.results.data;
+        this.currentPage = res.data.results.current_page;
+        this.lastPage = res.data.results.last_page;
+      })
+    }
+  },
+  mounted() {
+    this.getProjects(1);
+  }
+}
+</script>
+
+
+<style lang="scss" scoped></style>
---
+++ src/components/CardComponent.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="col-4">
+        <div class="card bg-dark text-white">
+            <div class="card-image">
+                <img :src="project.image" :alt="project.name">
+            </div>
+
+            <div class="card-body">
+                <h4>{{ project.name }}</h4>
+
+                <p class="fst-italic">{{ project.description }}</p>
+
+                <p>Technology: {{ project.technology.name }}</p>
+
+                <p>Tags:
+                    <span v-for="(tag, index) in project.tags" class="badge rounded-pill text-bg-danger text-white">
+                        <span v-if="tag.name.trim().length !== 0">{{ tag.name }}</span>
+                        <span v-else>Nessun tag</span>
+                    </span>
+                </p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'CardComponent',
+    props: ['project']
+}
+</script>
+
+<style lang="scss" scoped></style>
